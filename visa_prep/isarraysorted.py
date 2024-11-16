2a79397d498c135301ab30c0043f653361315806
sanjannagariakshitha <104752573+sanjannagariakshitha@users.noreply.github.com>
2024-11-16 22:40:48 +0530

    Update isarraysorted.py

--- visa_prep/isarraysorted.py
+++ visa_prep/isarraysorted.py
@@ -1,1 +1,8 @@
-
+n=int(input())
+arr=list(map(int, input().split()))
+is_sorted=True
+for i in range(1,n):
+    if arr[i]<arr[i-1]:
+        is_sorted=False
+        break
+print("true" if is_sorted else "false")
