c2cf1f5e3f4d410033c094062f64dddc30791765
sanjannagariakshitha <104752573+sanjannagariakshitha@users.noreply.github.com>
2024-11-16 23:01:08 +0530

    Update vigneshandgym.py

--- visa_prep/vigneshandgym.py
+++ visa_prep/vigneshandgym.py
@@ -1,1 +1,7 @@
-
+gym_cost, trainer_cost, monthly_bgt = map(int,input().split())
+if gym_cost > monthly_bgt:
+    print(0)
+elif gym_cost + trainer_cost <= monthly_bgt:
+    print(2)
+else:
+    print(1)
