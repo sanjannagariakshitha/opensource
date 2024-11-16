ab86d22b8be14e78ce7c10951139a10bd8a0980f
sanjannagariakshitha <104752573+sanjannagariakshitha@users.noreply.github.com>
2024-11-16 23:15:47 +0530

    Update best_coupon.py

--- visa_prep/best_coupon.py
+++ visa_prep/best_coupon.py
@@ -1,1 +1,6 @@
-
+bill_amount = int(input())
+flat_discount=100
+percentage_discount = bill_amount*0.1
+max_discount=max(percentage_discount, flat_discount)
+final_amount=bill_amount-max_discount
+print(int(final_amount))
