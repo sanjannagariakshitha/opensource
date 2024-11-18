f8f62f8ef9eed8662332a6831af5de28ffbc2405
sanjannagariakshitha <104752573+sanjannagariakshitha@users.noreply.github.com>
2024-11-18 19:15:47 +0530

    Update in_the_range_or_not.py

--- visa_prep/in_the_range_or_not.py
+++ visa_prep/in_the_range_or_not.py
@@ -1,1 +1,12 @@
-
+T = int(input())
+results = []
+min_freq = 67
+max_freq = 45000
+for _ in range(T):
+    X = int(input())
+    if min_freq <= X <= max_freq:
+        results.append("YES")
+    else:
+        results.append("NO")
+for result in results:
+    print(result)
